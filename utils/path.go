a0bec0871906bcc5027caf8a55d1f1b58ccd2b77
sinspired <ggmomo@gmail.com>
2026-09-12 00:35:17 +0800

    fix: 修复安卓环境配置路径权限问题

--- utils/path.go
+++ utils/path.go
@@ -1,16 +1,19 @@
+// utils/path.go
+//go:build !android && !ios
+
 package utils
 
 import (
-	"log/slog"
-	"os"
-	"path/filepath"
+    "log/slog"
+    "os"
+    "path/filepath"
 )
 
 func GetExecutablePath() string {
-	ex, err := os.Executable()
-	if err != nil {
-		slog.Error("获取程序路径失败", "error", err)
-		return "."
-	}
-	return filepath.Dir(ex)
+    ex, err := os.Executable()
+    if err != nil {
+        slog.Error("获取程序路径失败", "error", err)
+        return "."
+    }
+    return filepath.Dir(ex)
 }
